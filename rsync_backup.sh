9c3422370c3066e53d536c36546edb49a7d83534
olaulau <olaulau@gmail.com>
2022-04-24 00:06:50 +0200

    code indenting, less useless echo

--- rsync_backup.sh
+++ rsync_backup.sh
@@ -8,89 +8,87 @@
 
 
 function dst_request {
-# Effectue une requete sur la destination
-local RESULT
-if [ ${SERVER_IP} ]
-then # Destination SHH
-  RESULT=$( ssh ${LOGIN}@${SERVER_IP} -i ${RSA_KEY} -p ${PORT} "eval ${1}" )
-else # Destination locale
-  RESULT=$( eval ${1} )
-fi
-echo ${RESULT}
+	# Effectue une requete sur la destination
+	local RESULT
+	if [ ${SERVER_IP} ]
+	then # Destination SHH
+	  RESULT=$( ssh ${LOGIN}@${SERVER_IP} -i ${RSA_KEY} -p ${PORT} "eval ${1}" )
+	else # Destination locale
+	  RESULT=$( eval ${1} )
+	fi
+	echo ${RESULT}
 }
 
 
 function sauvegarde {
-
-# Effectue une sauvegarde
-
-#1- DEFINITION DE LA SAUVEGARDE COURANTE
-#TODAY_BACKUP="${DST_PTH}`date -I`"
-# little change to enable multiple backup per day
-TODAY_BACKUP="${DST_PTH}`date +%Y-%m-%d_%H-%M-%S`"
-echo "Folder to backup : ${SRC_PTH}"
-echo "Destination folder : ${TODAY_BACKUP}"
-
-#2- RECHERCHE DE LA DERNIERE SAUVEGARDE
-CMD="ls -1dr ${DST_PTH}*-*-* 2>/dev/null | head -1"
-LAST_BACKUP=$(dst_request "${CMD}")
-if [ -z ${LAST_BACKUP} ]
-then # Pas de sauvegarde anterieure
-  DUMMY_BACKUP="${DST_PTH}2000-01-01"
-  LAST_BACKUP="${DUMMY_BACKUP}"
-  dst_request "mkdir $DUMMY_BACKUP 2>/dev/null"
-  echo "Last backup : NONE"
-elif [ $LAST_BACKUP = $TODAY_BACKUP ]
-then # Une sauvegarde a deja ete effectue aujourd'hui
-  echo "ERROR: This folder has already been backed up today !"
-  exit 1
-else # Il y a une ou plusieurs sauvegardes anterieures
-  echo "Last backup : $LAST_BACKUP"
-fi
-# Creation du dossier de sauvegarde
-if [ $SIMULATION = 0 ]; then dst_request "mkdir -p $TODAY_BACKUP" ;fi
-
-#3- COMMANDE RSYNC
-OPTIONS="${DRYRUN_OPTION} ${RSYNC_OPTION} -avHh --stats --delete-after --delete-excluded"
-if [ -z ${SERVER_IP} ]
-then # Sauvegarde locale
-  rsync ${OPTIONS} ${EXCLUDE} --link-dest=${LAST_BACKUP} "${SRC_PTH}/" "${TODAY_BACKUP}/"  > ${LOG}
-else # Sauvegarde SSH
-  rsync ${OPTIONS} ${EXCLUDE} -e "ssh -i ${RSA_KEY} -p ${PORT}" --link-dest=${LAST_BACKUP} "${SRC_PTH}/" "${LOGIN}@${SERVER_IP}:${TODAY_BACKUP}/"  > ${LOG}
-fi
-
-# 4- NETTOYAGE DU LOG
-sed -e '/ file.../d' -e '/\/$/d' ${LOG} > ~/.log
-mv ~/.log ${LOG}
-
-
-# 5- EFFACEMENT DES VIEILLES SAUVEGARDES
-if [ ${DUMMY_BACKUP} ]
-then # Premiere sauvegarde 
-  dst_request "rm -rf ${DUMMY_BACKUP}"
-elif [ ${SIMULATION} = 0 ]
-then # Effacement des sauvegardes anterieurs
-  CMD="find ${DST_PTH}*-*-* -maxdepth 0 -type d -ctime +$HOLD"
-  OLD_FLD=$(dst_request "${CMD}")
-  dst_request "rm -rf ${OLD_FLD}"
-  if [ ${OLD_FLD} ]; then
-    echo "Deleted backup(s): ${OLD_FLD}"
-	echo ""
-  fi
-fi
-
-echo "Log file location: ${LOG}"
-echo "  $(grep "Number of files:" ${LOG})"
-echo "  $(grep "Total file size" ${LOG})"
-echo "  $(grep "Number of files transferred:" ${LOG})"
-echo "  $(grep "bytes/sec" ${LOG})"
+	# Effectue une sauvegarde
+	
+	#1- DEFINITION DE LA SAUVEGARDE COURANTE
+	#TODAY_BACKUP="${DST_PTH}`date -I`"
+	# little change to enable multiple backup per day
+	TODAY_BACKUP="${DST_PTH}`date +%Y-%m-%d_%H-%M-%S`"
+	echo "Folder to backup : ${SRC_PTH}"
+	echo "Destination folder : ${TODAY_BACKUP}"
+	
+	#2- RECHERCHE DE LA DERNIERE SAUVEGARDE
+	CMD="ls -1dr ${DST_PTH}*-*-*_*-*-* 2>/dev/null | head -1"
+	LAST_BACKUP=$(dst_request "${CMD}")
+	if [ -z ${LAST_BACKUP} ]
+	then # Pas de sauvegarde anterieure
+	  DUMMY_BACKUP="${DST_PTH}2000-01-01"
+	  LAST_BACKUP="${DUMMY_BACKUP}"
+	  dst_request "mkdir $DUMMY_BACKUP 2>/dev/null"
+	  echo "Last backup : NONE"
+	elif [ $LAST_BACKUP = $TODAY_BACKUP ]
+	then # Une sauvegarde a deja ete effectue aujourd'hui
+	  echo "ERROR: This folder has already been backed up today !"
+	  exit 1
+	else # Il y a une ou plusieurs sauvegardes anterieures
+	  echo "Last backup : $LAST_BACKUP"
+	fi
+	# Creation du dossier de sauvegarde
+	if [ $SIMULATION = 0 ]; then dst_request "mkdir -p $TODAY_BACKUP" ;fi
+	
+	#3- COMMANDE RSYNC
+	OPTIONS="${DRYRUN_OPTION} ${RSYNC_OPTION} -avHh --stats --delete-after --delete-excluded"
+	if [ -z ${SERVER_IP} ]
+	then # Sauvegarde locale
+	  rsync ${OPTIONS} ${EXCLUDE} --link-dest=${LAST_BACKUP} "${SRC_PTH}/" "${TODAY_BACKUP}/"  > ${LOG}
+	else # Sauvegarde SSH
+	  rsync ${OPTIONS} ${EXCLUDE} -e "ssh -i ${RSA_KEY} -p ${PORT}" --link-dest=${LAST_BACKUP} "${SRC_PTH}/" "${LOGIN}@${SERVER_IP}:${TODAY_BACKUP}/"  > ${LOG}
+	fi
+	
+	# 4- NETTOYAGE DU LOG
+	sed -e '/ file.../d' -e '/\/$/d' ${LOG} > ~/.log
+	mv ~/.log ${LOG}
+	
+	
+	# 5- EFFACEMENT DES VIEILLES SAUVEGARDES
+	if [ ${DUMMY_BACKUP} ]
+	then # Premiere sauvegarde 
+	  dst_request "rm -rf ${DUMMY_BACKUP}"
+	elif [ ${SIMULATION} = 0 ]
+	then # Effacement des sauvegardes anterieurs
+	  CMD="find ${DST_PTH}*-*-*_*-*-* -maxdepth 0 -type d -ctime +$HOLD"
+	  OLD_FLD=$(dst_request "${CMD}")
+	  dst_request "rm -rf ${OLD_FLD}"
+	  if [ ${OLD_FLD} ]; then
+	    echo "Deleted backup(s): ${OLD_FLD}"
+	  fi
+	fi
+	
+	echo "Log file location: ${LOG}"
+	#echo "  $(grep "Number of files:" ${LOG})"
+	#echo "  $(grep "Total file size" ${LOG})"
+	#echo "  $(grep "Number of files transferred:" ${LOG})"
+	#echo "  $(grep "bytes/sec" ${LOG})"
 }
 
 
 function restauration {
-# Effectue une restauration
-echo "This mode is not available yet !"
-exit 1
+	# Effectue une restauration
+	echo "This mode is not available yet !"
+	exit 1
 }
 
 
@@ -99,7 +97,6 @@ source ${1}
 EXCLUDE="${EXCLUDE}"" --exclude=*~ \
                       --exclude=*Thumbs.db \
                       --exclude=.DS_Store"
-echo ""
 
 #1- MODE SIMULATION
 if [ ${SIMULATION} = 1 ] ; then
@@ -124,5 +121,3 @@ else
   fi
 	restauration
 fi
-
-echo ""
